d558adb09a0c9fba89a52ce667fb86f05520b75b
ezdancewic <ezdancewic@smith.edu>
2024-09-24 16:17:33 -0400

    // completed assignment

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,120 @@
-class Conversation {
+import java.util.Scanner;
+import java.util.Random;
 
-  public static void main(String[] arguments) {
-    // You will start the conversation here.
+/**
+ * A class creating a chatbot conversation
+ */
+class Conversation {
+  // Attributes 
+  public static String mirrorWords[] = {"I", "me", "am", "you", "my", "your"};
+  public static String newWords[] = {"you", "you", "are", "I", "you", "my"};
+
+  // Constructor
+  public Conversation(){
   }
+
+
+  // Methods
+
+  /**
+   * A method taking the input string and breaking it into individual strings that are added to an array.
+   * @param response the string to be broken up
+   * @return Array of the broken up strings
+   */
+  public static String[] splitResponse(String response){
+    String[] separated_response = response.split(" ");
+    return separated_response;
+  }
+
+  /**
+   * A method checking if the given array contains any of the mirror words. 
+   * @param separated_response the array of strings obtained from the user's response 
+   * @return T/F whether the array contains mirror words or not
+   */
+  public static Boolean hasPronouns(String[] separated_response){
+    int arrayLength = separated_response.length;
+    for (int i = 0; i < arrayLength; i++){
+      for (int j = 0; j < 5; j++){
+        if (separated_response[i].equals(mirrorWords[j])){
+          return true;
+        }
+      } 
+    }
+    return false;
+  }
+
+  /**
+     * A method replacing mirror words with their corresponding replacements. 
+     * @param separated_response the array of strings obtained from the user's response. 
+     * @return string of the replaced words. 
+     */
+  public static String replaceWords(String[] separated_response){
+    int arrayLength = separated_response.length;
+    String replaced_Response = "";
+    if(hasPronouns(separated_response) == true){
+      for (int i = 0; i < arrayLength; i++){ 
+        if (separated_response[i].equals(mirrorWords[0])){
+          replaced_Response += newWords[0] + " ";
+        } 
+        else if (separated_response[i].equals(mirrorWords[1])){
+          replaced_Response += newWords[1] + " ";
+        } 
+        else if (separated_response[i].equals(mirrorWords[2])){
+          replaced_Response += newWords[2] + " ";
+        } 
+        else if (separated_response[i].equals(mirrorWords[3])){
+          replaced_Response += newWords[3] + " ";
+        } 
+        else if (separated_response[i].equals(mirrorWords[4])){
+          replaced_Response += newWords[4] + " ";
+        } 
+        else if (separated_response[i].equals(mirrorWords[5])){
+          replaced_Response += newWords[5] + " ";
+        } 
+        else {
+          replaced_Response += separated_response[i] + " ";
+        }  
+      }
+    }
+    return replaced_Response;
+  }
+
+
+  // Main
+  public static void main(String[] arguments) {
+    Scanner numRounds = new Scanner(System.in);
+    Scanner userResponse = new Scanner(System.in);
+    Random rand = new Random(); 
+    String[] cannedStrings = {"Hmm...", "Interesting...", "Wow!", "I see."};
+    int rounds = -1;
+    String response = "";
+    String transcript = "TRANSCRIPT" + "\n";
+    
+
+    // Start conversation
+    System.out.println("How many rounds:");
+    rounds = numRounds.nextInt();
+    System.out.println("Hi! What's on your mind?");
+    for (int currentRound = 0; currentRound < rounds; currentRound++) {
+      response = userResponse.nextLine();
+      transcript += response + "\n";
+      //System.out.println(splitResponse(response));
+      if(hasPronouns(splitResponse(response)) == true){
+        String computer_response = replaceWords(splitResponse(response));
+        System.out.println(computer_response);
+        transcript += computer_response + "\n";
+      }
+      else{
+        int rand_int1 = rand.nextInt(4);
+        System.out.println(cannedStrings[rand_int1]);
+        transcript += cannedStrings[rand_int1] + "\n";
+      }
+    }
+    System.out.println("Goodbye!");
+    transcript += "Goodbye!" + "\n";
+
+    System.out.println(transcript);
+    numRounds.close();
+    userResponse.close();
+}
 }
--- reflection.md
+++ reflection.md
@@ -1,7 +1,16 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+I consulted with Nicole Choi, who is in section 2 of CSC-120. Because she was in a different section, she was drawing off of a slightly different version of the GuessingGame example, and we ended up implementing different approaches to defining the scanners and getting the initial inputs. This was super helpful because we were able to help each other without having the exact same code and I ended up understanding both why her method worked and why mine did. I also consulted Emily Middleton who is in Section 1. We helped each other debug our MirrorWord functions and she helped me realize why doing the whole thing with if statements was gonna be logistically messy. 
+
 - Which session(s) of TA / office hours did you attend?
+I went to TA hours on Thursday, Sept 19th 8-9, Sunday Sept 22nd 2-3, and Monday Sept 23rd 7-9. 
+
 - What are your initial impressions of Java? 
+It's not too bad. Still not used to some of the syntax, and its hard to wrap my head around object oriented programming, but I'm getting there. 
+
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+Programming in general has some pretty universal concepts (for loops & iteration, conditionals, types, logical sequences), but each language deals with their syntax and how the data are stored differently. 
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+I felt that it was incredibly necessary to go to TA hours on this assignment (perhaps to the point that I needed them too much). I could understand how to do the mirror words conceptually, and write out the code for it, but if I got an error I couldn't figure out on my own, or needed to know some specific method, it felt like I *couldn't* make progress until I got help from a TA. Generally though, it was extremely helpful to discuss this assignment with my peers. I would recommend future students to map out their code on paper if they get a response they don't expect. 
